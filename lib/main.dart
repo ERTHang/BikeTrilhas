2eaf4dc2033f77b942ffb6d1d739b93280ad975c
Victor Eduardo Requia <victorrequia@gmail.com>
2021-03-24 11:39:46 -0300

    Update 1.3.5
    * Corrigido bug no qual as trilhas salvas não estavam aparecendo no primeiro momento em verde assim como as informações

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:biketrilhas_modular/app/app_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'app/app_widget.dart';
+import 'app/shared/info/save_trilha.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await getPrefNoAlert();
   runApp(ModularApp(
     child: AppWidget(),
     module: AppModule(),
